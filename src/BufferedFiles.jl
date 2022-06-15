652c462b5f223b5b1e4eec90ea2436f8475f338c
Paul Depping <paul.depping@gmail.com>
2022-06-15 14:50:01 +0200

    Complete Refactor for version 4.0.0

--- src/BufferedFiles.jl
+++ src/BufferedFiles.jl
@@ -1,355 +1,236 @@
 """
     BufferedFiles
-
-Implementations of a better Filehandles.
-
-Includes a Read-Only Filehandle and a better Write-Only Filehandle
-
-Uses Julias Filesystem.File for Portability.
-
-Main Functions: [`open`](@ref) and [`@om_str`](@ref)
 """
 module BufferedFiles
+
 using Base.Filesystem
 
-export @om_str, bufferedopen
+abstract type OpenMode end
 
-function cmemcpy(dest, src, nbytes)
-    @ccall memcpy(dest::Ptr{Cvoid}, src::Ptr{Cvoid}, nbytes::Csize_t)::Ptr{Cvoid}
-    nothing
-end
+export @om_str
 
-function cmemmove(dest, src, nbytes)
-    @ccall memmove(dest::Ptr{Cvoid}, src::Ptr{Cvoid}, nbytes::Csize_t)::Ptr{Cvoid}
-    nothing
-end
+struct ReadOnly <: OpenMode end
+struct WriteTruncOnly <: OpenMode end
+struct WriteOnly <: OpenMode end
 
-"""
-    const DEFAULT_BUFSIZE = 16 * (1024) * (1024)
-
-Default size of the read/write-buffer of [`BufferedReadFile`](@ref) and [`BufferedWriteFile`](@ref).
-"""
-const DEFAULT_BUFSIZE = 16 * (1024) * (1024)
-
-"""
-    abstract type BufferedOpenMode end
-
-Types Implemented by [`@om_str`](@ref), used in [`open`](@ref)
-"""
-abstract type BufferedOpenMode end
-
-struct Read <: BufferedOpenMode end
-
-struct WriteTrunc <: BufferedOpenMode end
-
-struct Append <: BufferedOpenMode end
-
-struct ReadWrite <: BufferedOpenMode end
-
-struct ReadWriteTrunc <: BufferedOpenMode end
-
-struct ReadAppend <: BufferedOpenMode end
-
-struct MmapRead <: BufferedOpenMode end
-
-struct MmapReadWrite <: BufferedOpenMode end
-
-"""
-    BufferedOpenMode(pattern::AbstractString)
-
-Convert `pattern` into its corresponding subtype of `BufferedOpenMode`.
-
-Recommended Usage of this is with the [`@om_str`](@ref) macro at parse-time.
-"""
-function BufferedOpenMode(pattern::AbstractString)
-    if pattern == "r"
-        return Read()
-    elseif pattern == "w"
-        return WriteTrunc()
-    elseif pattern == "a"
-        return Append()
-    elseif pattern == "r+"
-        return ReadWrite()
-    elseif pattern == "w+"
-        return ReadWriteTrunc()
-    elseif pattern == "a+"
-        return ReadAppend()
-    elseif pattern == "mr"
-        return MmapRead()
-    elseif pattern == "mr+"
-        return MmapReadWrite()
+function string2openmode(s::AbstractString)
+    if s == "r"
+        return ReadOnly()
+    elseif s == "w"
+        return WriteTruncOnly()
+    else
+        throw(ArgumentError("no matching openmode found"))
     end
-    return error("Error, no matching string found!")
 end
 
-"""
-    @om_str "pattern"
-    om"pattern"
-
-Convert `pattern` into its corresponding subtype of [`BufferedOpenMode`](@ref) at parse-time.
-"""
-macro om_str(pattern)
-    return BufferedOpenMode(pattern)
+macro om_str(s::String)
+    return string2openmode(s)
 end
 
-"""
-    abstract type BufferedFile <: IO end
+mutable struct BufferedReadFile <: IO
+    file::File
+    filepos::UInt
+    bufpos::UInt
+    bufread::UInt
+    total_filesize::UInt
+    buf::Vector{UInt8}
+    function BufferedReadFile(args...)
+        f = new(args...)
+        finalizer(close, f)
+        return f
+    end
+end
 
-Abstract Type for my Filehandles, to overwrite some of Julias functions from io.jl that cause allocations.
-"""
-abstract type BufferedFile <: IO end
+function memcpy(dest, src, nbytes)
+    @ccall memcpy(dest::Ptr{Cvoid}, src::Ptr{Cvoid}, nbytes::Csize_t)::Ptr{Cvoid}
+    return
+end
 
-const bufferedopen = Base.open
+function memmove(dest, src, nbytes)
+    @ccall memmove(dest::Ptr{Cvoid}, src::Ptr{Cvoid}, nbytes::Csize_t)::Ptr{Cvoid}
+    return
+end
+
+Base.eof(r::BufferedReadFile) = r.total_filesize == r.filepos && r.bufpos == r.bufread
+
+const DEFAULT_BUFSIZE = 1024 * 1024 # 1MB
+
+function Base.open(fname::AbstractString, ::ReadOnly, bufsize::Integer=DEFAULT_BUFSIZE)
+    buf = Vector{UInt8}(undef, bufsize)
+    file = Filesystem.open(fname, JL_O_RDONLY)
+    fs = filesize(file)
+    return BufferedReadFile(file, 0, 0, 0, fs, buf)
+end
+
+function Base.bytesavailable(r::BufferedReadFile)
+    return r.bufread - r.bufpos
+end
+
+function move_old(r::BufferedReadFile)
+    "Move existing, not yet read Bytes to the beginning of the buffer"
+    ncopy = bytesavailable(r)
+    buf = r.buf
+    GC.@preserve buf memmove(pointer(buf), pointer(buf) + r.bufpos, ncopy)
+    r.bufpos = 0
+    r.bufread = ncopy
+    return
+end
+
+"Read in new Bytes"
+function read_new(r::BufferedReadFile)
+    buf = r.buf
+    to_read = min((length(buf) % UInt - r.bufread), r.total_filesize - r.filepos)::UInt
+    GC.@preserve buf unsafe_read(r.file, pointer(buf) + r.bufread, to_read)
+    r.filepos += to_read
+    r.bufread += to_read
+    return
+end
+
+function refresh(r::BufferedReadFile)
+    move_old(r)
+    read_new(r)
+    return r
+end
+
+function Base.seek(r::BufferedReadFile, off::Integer)
+    uOff::UInt = off
+    offstart = r.filepos - r.bufread
+    offend = r.filepos
+    if offstart <= uOff <= offend # is within read buffer
+        r.bufpos = r.filepos - uOff
+    else
+        seek(r.file, uOff)
+        r.bufpos = 0
+        r.bufread = 0
+        r.filepos = uOff
+    end
+    return r
+end
+
+function Base.position(r::BufferedReadFile)
+    return r.filepos - (r.bufread - r.bufpos)
+end
+
+function Base.skip(r::BufferedReadFile, rel::Integer)
+    return seek(r, (position(rel) % (rel isa Signed ? Int : UInt)) + rel)
+end
+
+function Base.read(r::BufferedReadFile, ::Type{UInt8})
+    isopen(r.file) || error("file is closed")
+    if r.bufpos < r.bufread
+        r.bufpos += UInt(1)
+        return r.buf[r.bufpos]
+    else
+        "Have to attempt to read more from file"
+        refresh(r)
+        if r.bufpos < r.bufread
+            r.bufpos += UInt(1)
+            return r.buf[r.bufpos]
+        else
+            throw(EOFError())
+        end
+    end
+
+    error("how did control flow get here?")
+end
+
+function Base.resize!(r::BufferedReadFile, ns::Integer)
+    uOff = position(r)
+    resize!(r.buf, ns)
+    r.bufpos = 0
+    r.bufread = 0
+    seek(r.file, uOff)
+    r.filepos = uOff
+    return r
+end
+
+function Base.unsafe_read(r::BufferedReadFile, p::Ptr{UInt8}, n::UInt)
+    isopen(r.file) || error("file is closed")
+    if n > 0 && eof(r)
+        throw(EOFError())
+    end
+    buf = r.buf
+    ba = bytesavailable(r)
+    if ba >= n # usually can just copy from memory
+        GC.@preserve buf memcpy(p, pointer(buf) + r.bufpos, n)
+        r.bufpos += n
+    else
+        GC.@preserve buf memcpy(p, pointer(buf) + r.bufpos, ba)
+        r.bufpos += ba
+
+        "if the amount of data to be read is larger than the rest of the buffer, just read straight from file"
+        if n - ba > (length(buf) % UInt)
+            r.bufpos = 0
+            r.bufread = 0
+            r.filepos += n - ba
+            unsafe_read(r.file, p + ba, n - ba)
+        else
+            refresh(r)
+            unsafe_read(r, p + ba, n - ba)
+        end
+    end
+    return
+end
+
+function Base.close(r::BufferedReadFile)
+    resize!(r.buf, 0)
+    sizehint!(r.buf, 0)
+    r.bufpos = 0
+    r.bufread = 0
+    r.total_filesize = 0
+    if isopen(r.file)
+        close(r.file)
+    end
+    return r
+end
 
 # Overwrites two @noinline julia functions which cause allocations in a lot of situations
-function Base.unsafe_read(s::BufferedFile, p::Ref{T}, n::Integer) where {T}
+function Base.unsafe_read(s::BufferedReadFile, p::Ref{T}, n::Integer) where {T}
     GC.@preserve p unsafe_read(s, Base.unsafe_convert(Ref{T}, p)::Ptr, n)
 end
-function Base.unsafe_write(s::BufferedFile, p::Ref{T}, n::Integer) where {T}
-    GC.@preserve p unsafe_write(s, Base.unsafe_convert(Ref{T}, p)::Ptr, n)
+function Base.unsafe_read(s::BufferedReadFile, p::Ptr, n::Integer)
+    return unsafe_read(s, convert(Ptr{UInt8}, p)::Ptr{UInt8}, convert(UInt, n)::UInt)
 end
 
-function Base.unsafe_read(s::BufferedFile, p::Ptr, n::Integer)
-    return unsafe_read(s, convert(Ptr{UInt8}, p), convert(UInt, n))
+mutable struct BufferedWriteFile <: IO
+    file::File
+    filepos::Int
+    bufpos::Int
+    buf::Vector{UInt8}
+    function BufferedWriteFile(args...)
+        f = new(args...)
+        finalizer(close, f)
+        return f
+    end
 end
-function Base.unsafe_write(s::BufferedFile, p::Ptr, n::Integer)
+
+function Base.unsafe_write(s::BufferedWriteFile, p::Ref{T}, n::Integer) where {T}
+    GC.@preserve p unsafe_write(s, Base.unsafe_convert(Ref{T}, p)::Ptr, n)
+end
+function Base.unsafe_write(s::BufferedWriteFile, p::Ptr, n::Integer)
     return unsafe_write(s, convert(Ptr{UInt8}, p), convert(UInt, n))
 end
 
-"""
-    BufferedReadFile
-
-Buffered Filehandle for more performant Reading.
-
-Opened via [`open(filename, om"r")`](@ref Base.open(::AbstractString, ::Read)).
-"""
-mutable struct BufferedReadFile <: BufferedFile
-    rf::File
-    buf::Vector{UInt8}
-    filepos::Int
-    fileend::Int
-    bufpos::Int
-    buflastread::Int
-    function BufferedReadFile(rf, buf)
-        f = new(rf, buf, position(rf), filesize(rf), 0, 0)
-        finalizer(close, f)
-        refresh(f)
-        return f
-    end
-end
-
-Base.isopen(p::BufferedReadFile) = isopen(p.rf)
-Base.isreadable(p::BufferedReadFile) = isopen(p)
-Base.iswritable(::BufferedReadFile) = false
-
-"""
-    open(file::AbstractString, om"r"[, bufsize::Integer])
-
-Open a File in Read-Only Mode.
-
-Returns a [`BufferedReadFile`](@ref).
-"""
-function Base.open(file::AbstractString, ::Read, bufsize::Integer=DEFAULT_BUFSIZE)
-    rf = Filesystem.open(file, JL_O_RDONLY)
-    buf = Vector{UInt8}(undef, bufsize)
-    f = BufferedReadFile(rf, buf)
-    return f
-end
-
-function refresh(f::BufferedReadFile)
-    GC.@preserve f begin
-        cmemmove(pointer(f.buf), pointer(f.buf) + f.bufpos, bytesavailable(f))
-        f.buflastread = bytesavailable(f)
-        f.bufpos = 0
-        to_read::UInt = min(length(f.buf) - f.buflastread, max(0, f.fileend - f.filepos))
-        f.filepos += to_read
-        unsafe_read(f.rf, pointer(f.buf) + f.buflastread, to_read)
-        f.buflastread += to_read
-    end
-    return f
-end
-
-function Base.position(f::BufferedReadFile)::Int
-    return f.filepos - (f.buflastread - f.bufpos)
-end
-
-function Base.eof(f::BufferedReadFile)::Bool
-    return (f.buflastread - f.bufpos) == 0
-end
-
-function Base.skip(f::BufferedReadFile, n::Integer)
-    todo::UInt = bytesavailable(f)
-    if n >= 0
-        nb::UInt = n
-        if todo == 0
-            if nb > 0
-                throw(EOFError())
-            end
-        elseif todo > nb
-            f.bufpos += nb
-        elseif todo <= nb
-            f.bufpos = f.buflastread
-            refresh(f)
-            skip(f, (nb - todo)::UInt)
-        end
-    else
-        u::UInt = abs(n)
-        if f.bufpos < u
-            dif = u - f.bufpos
-            skip(f.rf, -Int(f.buflastread + dif))
-            f.filepos -= (f.buflastread + dif)
-            f.bufpos = 0
-            f.buflastread = 0
-            refresh(f)
-        else
-            f.bufpos -= u
-        end
-    end
-    return f
-end
-
-function Base.seek(f::BufferedReadFile, n::Integer)
-    if position(f) != n
-        seek(f.rf, n)
-        f.filepos = n
-        f.buflastread = 0
-        f.bufpos = 0
-        refresh(f)
-    end
-    return f
-end
-
-function Base.close(f::BufferedReadFile)
-    if isopen(f)
-        close(f.rf)
-        resize!(f.buf, 0)
-        sizehint!(f.buf, 0)
-    end
-    return nothing
-end
-
-Base.bytesavailable(f::BufferedReadFile) = f.buflastread - f.bufpos
-
-function Base.read(s::BufferedReadFile, ::Type{UInt8})
-    r = bytesavailable(s)
-    if r >= 2
-        s.bufpos += 1
-        return @inbounds s.buf[s.bufpos]
-    elseif r == 1
-        s.bufpos += 1
-        cnt = @inbounds s.buf[s.bufpos]
-        refresh(s)
-        return cnt
-    elseif r == 0
-        throw(EOFError())
-    else
-        error("this should never happen!")
-    end
-end
-
-function Base.unsafe_read(f::BufferedReadFile, p::Ptr{UInt8}, nb::UInt)
-    todo::UInt = bytesavailable(f)
-    if todo == 0
-        if nb > 0
-            throw(EOFError())
-        else
-            return nothing
-        end
-    end
-    if todo > nb
-        GC.@preserve f begin
-            cmemcpy(p, pointer(f.buf) + f.bufpos, nb)
-        end
-        f.bufpos += nb
-        return nothing
-    elseif todo < nb
-        GC.@preserve f begin
-            cmemcpy(p, pointer(f.buf) + f.bufpos, todo)
-        end
-        f.bufpos = f.buflastread
-        refresh(f)
-        return unsafe_read(f, p + todo, nb - todo)
-    elseif todo == nb
-        GC.@preserve f begin
-            cmemcpy(p, pointer(f.buf) + f.bufpos, todo)
-        end
-        f.bufpos = f.buflastread
-        refresh(f)
-        return nothing
-    else
-        error("This should never happen!")
-    end
-end
-
-function Base.resize!(b::BufferedReadFile, ns::Integer)
-    s = Int(ns)
-    if b.buflastread < s
-        resize!(b.buf, s)
-    elseif b.buflastread >= s
-        p = position(b)
-        seek(b.rf, p)
-        b.filepos = p
-        b.buflastread = 0
-        b.bufpos = 0
-        resize!(b.buf, s)
-        refresh(b)
-    end
-    return b
-end
-
-"""
-    BufferedWriteFile
-
-Buffered Filehandle for more performant sequential Writing.
-
-Opened via [`open(filename, om"r")`](@ref open(::AbstractString, ::Read)).
-"""
-mutable struct BufferedWriteFile <: BufferedFile
-    rf::File
-    buf::Vector{UInt8}
-    filepos::Int
-    bufpos::Int
-    function BufferedWriteFile(rf, buf)
-        f = new(rf, buf, position(rf), 0)
-        finalizer(close, f)
-        return f
-    end
-end
-
-function Base.resize!(b::BufferedWriteFile, ns::Integer)
-    s = Int(ns)
-    if s <= b.bufpos
-        flush(b)
-    end
-    resize!(b.buf, s)
-    return b
-end
-
-Base.isopen(p::BufferedWriteFile) = isopen(p.rf)
-Base.isreadable(::BufferedWriteFile) = false
-Base.iswritable(p::BufferedWriteFile) = isopen(p)
-
-"""
-    open(file::AbstractString, om"w"[, bufsize::Integer])
-
-Open a File in Write-Only Mode.
-
-Truncates the File when opening it.
-
-Returns a [`BufferedWriteFile`](@ref).
-"""
 function Base.open(file::AbstractString,
-                   ::WriteTrunc,
+                   ::WriteTruncOnly,
                    bufsize::Integer=DEFAULT_BUFSIZE)
     buf = Vector{UInt8}(undef, bufsize)
-    rf = Filesystem.open(file, JL_O_CREAT | JL_O_TRUNC | JL_O_WRONLY,
-                         S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-    return BufferedWriteFile(rf, buf)
+    file = Filesystem.open(file, JL_O_CREAT | JL_O_TRUNC | JL_O_WRONLY,
+                           S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+    return BufferedWriteFile(file, 0, 0, buf)
+end
+
+function Base.open(file::AbstractString,
+                   ::WriteOnly,
+                   bufsize::Integer=DEFAULT_BUFSIZE)
+    buf = Vector{UInt8}(undef, bufsize)
+    file = Filesystem.open(file, JL_O_WRONLY)
+    return BufferedWriteFile(file, 0, 0, buf)
 end
 
 function Base.flush(f::BufferedWriteFile)
     GC.@preserve f begin
-        unsafe_write(f.rf, pointer(f.buf), f.bufpos)
+        unsafe_write(f.file, pointer(f.buf), f.bufpos)
     end
     f.filepos += f.bufpos
     f.bufpos = 0
@@ -360,55 +241,24 @@ function Base.position(f::BufferedWriteFile)::Int
     return f.filepos + f.bufpos
 end
 
-function Base.skip(f::BufferedWriteFile, n::Integer)
-    flush(f)
-    skip(f.rf, n)
-    f.filepos += n
-    return f
-end
-
 function Base.seek(f::BufferedWriteFile, n::Integer)
-    if position(f) != n
+    if f.filepos <= n <= position(f) # position within buffer
+        f.bufpos = n - f.filepos
+    else
         flush(f)
-        seek(f.rf, n)
+        seek(f.file, n)
+        f.filepos = n
     end
-    f.filepos = n
     return f
 end
 
-function Base.close(f::BufferedWriteFile)
-    if isopen(f)
-        flush(f)
-        close(f.rf)
-        resize!(f.buf, 0)
-        sizehint!(f.buf, 0)
-    end
-    return nothing
-end
-
-function Base.unsafe_write(f::BufferedWriteFile, p::Ptr{UInt8}, nb::UInt)::UInt
-    freespace = (length(f.buf) - f.bufpos)
-    @assert freespace > 0
-    if freespace > nb
-        GC.@preserve f begin
-            cmemcpy(pointer(f.buf) + f.bufpos, p, nb)
-        end
-        f.bufpos += nb
-        return nb
-    elseif freespace <= nb
-        GC.@preserve f begin
-            cmemcpy(pointer(f.buf) + f.bufpos, p, freespace)
-        end
-        f.bufpos += freespace
-        flush(f)
-        return freespace + unsafe_write(f, p + freespace, nb - freespace)
-    else
-        error("This should never happen!")
-    end
+function Base.skip(f::BufferedWriteFile, n::Integer)
+    seek(f, (position(f) % Int) + n)
 end
 
 function Base.write(s::BufferedWriteFile, u::UInt8)
-    freespace = (length(s.buf) - s.bufpos)
+    isopen(s.file) || error("file is closed")
+    freespace::UInt = (length(s.buf) - s.bufpos)
     if freespace >= 2
         s.bufpos += 1
         @inbounds s.buf[s.bufpos] = u
@@ -421,5 +271,48 @@ function Base.write(s::BufferedWriteFile, u::UInt8)
     end
     return 1
 end
+
+function Base.unsafe_write(f::BufferedWriteFile, p::Ptr{UInt8}, nb::UInt)::UInt
+    isopen(f.file) || error("file is closed")
+    freespace::UInt = (length(f.buf) - f.bufpos)
+    @assert freespace > 0
+    if freespace > nb
+        GC.@preserve f begin
+            memcpy(pointer(f.buf) + f.bufpos, p, nb)
+        end
+        f.bufpos += nb
+        return nb
+    elseif freespace <= nb
+        GC.@preserve f begin
+            memcpy(pointer(f.buf) + f.bufpos, p, freespace)
+        end
+        f.bufpos += freespace
+        flush(f)
+        return freespace + unsafe_write(f, p + freespace, nb - freespace)
+    else
+        error("This should never happen!")
+    end
+end
+
+function Base.resize!(f::BufferedWriteFile, ns::Integer)
+    flush(f)
+    resize!(f.buf, ns)
+end
+
+function Base.close(f::BufferedWriteFile)
+    if isopen(f.file)
+        flush(f)
+        close(f.file)
+    end
+end
+
+#=
+function Base.unsafe_write(s::BufferedFile, p::Ptr, n::Integer)
+    return unsafe_write(s, convert(Ptr{UInt8}, p), convert(UInt, n))
+end
+function Base.unsafe_write(s::BufferedFile, p::Ref{T}, n::Integer) where {T}
+    GC.@preserve p unsafe_write(s, Base.unsafe_convert(Ref{T}, p)::Ptr, n)
+end
+=#
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ end
 cd("testdir")
 
 @testset "BufferedWriteFile" begin
-    for i in 1:1024
+    for i in 1:100
         f = open("testfile$i", om"w", rand(1:20))
         @test f isa BufferedFiles.BufferedWriteFile
         @test position(f) == 0
@@ -18,7 +18,7 @@ cd("testdir")
         @test position(f) == 16
         @test write(f, 0x01) == 1
         @test position(f) == 17
-        resize!(f, rand(1:20))
+        resize!(f, rand(1:100))
         @test write(f, UInt8[1, 2, 3, 4, 5, 6, 7, 8]) == 8
         @test position(f) == 25
         @test write(f, 1) == 8
@@ -26,12 +26,15 @@ cd("testdir")
         @test write(f, 0x01) == 1
         @test position(f) == 34
         close(f)
+        @test read("testfile$i") ==
+              UInt8[1, 2, 3, 4, 5, 6, 7, 8, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 7,
+                    8, 1, 0, 0, 0, 0, 0, 0, 0, 1]
     end
     v = read("testfile1")
 end
 
 @testset "BufferedReadFile" begin
-    for i in 1:1024
+    for i in 1:100
         f = BufferedFiles.open("testfile$i", om"r", rand(1:20))
         @test f isa BufferedFiles.BufferedReadFile
         @test eof(f) == false
@@ -44,7 +47,8 @@ end
         @test eof(f) == false
         @test read(f, UInt8) == 1
         @test position(f) == 17
-        resize!(f, rand(1:20))
+        resize!(f, rand(1:100))
+        @test position(f) == 17
         @test read(f, 8) == UInt8[1, 2, 3, 4, 5, 6, 7, 8]
         @test position(f) == 25
         @test read(f, Int) == 1
@@ -53,6 +57,7 @@ end
         @test position(f) == 34
         @test eof(f) == true
         @test_throws EOFError read(f, UInt8)
+        @test_throws EOFError read(f, UInt16)
         close(f)
     end
 end
